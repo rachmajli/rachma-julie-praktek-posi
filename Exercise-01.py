5e09d6661fe3e3cd3906c483fef638b6ba0aa52f
rachmajli <rachmajulienuraini@gmail.com>
2023-03-13 00:06:47 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,12 @@
+nama = "Rachma Julie Nuraini"
+nim = "I0322099"
+tempat_tinggal = "Magetan"
+visualisasi = "Magetan merupakan kabupaten kecil yang berada di kaki Gunung Lawu, berbatasan dengan Jawa Tengah, setiap musim hujan udaranya selalu dingin"
+harapan = "Harapan saya setelah lulus dari Teknik Industri ialah bekerja di bidang yang sesuai dengan matkul di Teknik Industri dan sesuai minat sekaligus cita cita selama ini"
+print()
+
+print(nama)
+print(nim)
+print(tempat_tinggal)
+print(visualisasi)
+print(harapan)
